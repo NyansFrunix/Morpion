c23cfdb56b982cc1f140e22a721d492dc2cf0422
NyansFrunix <105979573+NyansFrunix@users.noreply.github.com>
2022-09-30 23:29:58 +0200

    Add files via upload

---
+++ morpion.py
@@ -0,0 +1,23 @@
+from turtle import *
+speed(5)
+pu()
+goto(0,0)
+pd()
+forward(99)
+
+pu()
+goto(0,33)
+pd()
+forward(99)
+
+pu()
+goto(33,-33)
+right(-90)
+pd()
+forward(99)
+
+pu()
+goto(66,-33)
+pd()
+forward(99)
+
